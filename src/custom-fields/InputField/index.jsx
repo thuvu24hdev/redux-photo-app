84042533a4c34ff4b67050a9e6ceba1bc80eb484
VuThiBichThu <thuvu.24hdev@gmail.com>
2021-11-04 16:52:52 +0700

    Add form validation with Yup

--- src/custom-fields/InputField/index.jsx
+++ src/custom-fields/InputField/index.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { FormGroup, Input, Label } from 'reactstrap';
+import { FormFeedback, FormGroup, Input, Label } from 'reactstrap';
+import { ErrorMessage } from 'formik';
 
 InputField.propTypes = {
   field: PropTypes.object.isRequired,
@@ -21,11 +22,12 @@ InputField.defaultProps = {
 
 function InputField(props) {
   const {
-    field,
+    field, form,
     type, label, placeholder, disabled,
   } = props;
   const { name } = field;
-
+  const { errors, touched } = form;
+  const showError = errors[name] && touched[name];
   return (
     <FormGroup>
       {label && <Label for={name}>{label}</Label>}
@@ -37,7 +39,9 @@ function InputField(props) {
         type={type}
         disabled={disabled}
         placeholder={placeholder}
+        invalid={showError}
       />
+      <ErrorMessage name={name} component={FormFeedback} />
     </FormGroup>
   );
 }
